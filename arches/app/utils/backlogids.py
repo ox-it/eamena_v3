57bcbf87ffec1c590272429551dd0c2f6808a307
azerbini <andrea.zerbini@arch.ox.ac.uk>
2016-09-16 10:56:51 +0100

    Proved backlogids.py
    Relies on Resource().index() for reindexing instead of using its own
    method.

--- arches/app/utils/backlogids.py
+++ arches/app/utils/backlogids.py
@@ -12,14 +12,26 @@ from django.conf import settings
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.geos import fromstr
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+from django.core.exceptions import ObjectDoesNotExist
 
 def createBacklogIds():
-    entitytype = archesmodels.EntityTypes.objects.get(pk = "HERITAGE_RESOURCE_GROUP.E27")
-    type = 'EAMENA'
-    entities = archesmodels.Entities.objects.filter(entitytypeid__exact = entitytype)
+    entitytype = archesmodels.EntityTypes.objects.get(pk = "ACTOR.E39")
+    type = 'ACTOR'
+    all_entities = archesmodels.Entities.objects.filter(entitytypeid__exact = entitytype)
+    entities =[]
+    errors = []
+    for count, entity in enumerate(all_entities, 1):
+        if count % 5000 == 0:
+            print "%s resources inspected" % count
+        try:
+            relation = archesmodels.Relations.objects.get(ruleid=archesmodels.Rules.objects.get(entitytypedomain=entitytype, entitytyperange="EAMENA_ID.E42").ruleid, entityiddomain =entity.entityid)
+        except  ObjectDoesNotExist:
+            entities.append(entity)
+            
+    print "There are %s resources and %s which do not have a EAMENA_ID.E42" % (all_entities.count(), len(entities))
 
     for count, entity in enumerate(entities, 1):
-        if count % 500 == 0:
+        if count % 1000 == 0:
             print "%s UniqueIds created" % count
         entity2 = archesmodels.Entities()
         entity2.entitytypeid = archesmodels.EntityTypes.objects.get(pk = "EAMENA_ID.E42")
@@ -35,107 +47,25 @@ def createBacklogIds():
             lastID = uniqueidmodel.objects.filter(id_type__exact=type).latest()
             IdInt = int(lastID.val) + 1
             uniqueidmodelinstance.val = str(IdInt)
-            
-        except:
+        except ObjectDoesNotExist:
+            print "The resource %s has been assigned the first ID with entityid %s" % (entity.entityid,entity2.entityid)
             uniqueidmodelinstance.val = str(1)
             
         uniqueidmodelinstance.order_date = datetime.datetime.now()
         uniqueidmodelinstance.save()
-        
-        
-
+            
+            
+    
         zerosLength = settings.ID_LENGTH if  settings.ID_LENGTH > len(uniqueidmodelinstance.val) else len(uniqueidmodelinstance.val)
         value = type +"-"+uniqueidmodelinstance.val.zfill(zerosLength)
         
-        ReindexResource(entity.entityid, entity2.entityid, value)
-        
-        
-def ReindexResource(entityid1, entityid2, value):
-    document = Entity()
-    document.property = ''
-    document.entitytypeid = 'HERITAGE_RESOURCE_GROUP.E27'
-    document.entityid = entityid1
-    document.value = ''
-    document.label = ''
-    document.businesstablename = ''
-    document.primaryname = value
-    document.child_entities = []
-    document.dates = []
-    document.domains = []
-    document.geometries = []
-    document.numbers = []
-
-    
-
-    def find_nested(document,domain,topId = None, topType = None, idvalue=None):
-        relations = archesmodels.Relations.objects.filter(entityiddomain__exact = domain)
-        for relation in relations:
-            entityinstance = Entity()
-            entityinstance.value = ''
-            entityinstance.label = ''               
-            entityinstance.child_entities = []         
-            entityinstance.entitytypeid = topType
-            entityinstance.parentid = topId
-            entityinstance.property = ''
-            entityinstance.businesstablename = ''
-            
-            entity = archesmodels.Entities.objects.get(pk=relation.entityidrange_id)
-            entitytype = archesmodels.EntityTypes.objects.get(pk = entity.entitytypeid_id)
-
-            rule = archesmodels.Rules.objects.get(entitytyperange__exact = entitytype, ruleid = relation.ruleid_id)
-            entityinstance.entitytypeid = entitytype.entitytypeid
-            entityinstance.parentid = relation.entityiddomain_id
-            entityinstance.entityid = entity.entityid
-            entityinstance.property = rule.propertyid_id 
-            entityinstance.businesstablename = entitytype.businesstablename
-            if entitytype.businesstablename == 'domains':
-                value = archesmodels.Domains.objects.get(entityid = entity.entityid)
-                entityinstance.conceptid = entitytype.conceptid_id
-                entityinstance.label = archesmodels.Values.objects.get(pk = value.val_id).value
-                entityinstance.value = value.val_id
+#         ReindexResource(entity.entityid, entity2.entityid, value)
+        try:
+            resource = Resource().get(entity.entityid)
+            resource.index()
+        except:
+            if e not in errors:
+                errors.append(e)
                 
-                document.domains.append(entityinstance)
-            elif entitytype.businesstablename == 'dates':
-                value = archesmodels.Dates.objects.get(entityid = entity.entityid)
-                entityinstance.value = value.val
-                entityinstance.label = value.val           
-                document.dates.append(entityinstance)
-            elif entitytype.businesstablename == 'geometries':
-                value = archesmodels.Geometries.objects.get(entityid = entity.entityid)
-                entityinstance.value = JSONDeserializer().deserialize(fromstr(value.val.geojson).json)         
-                entityinstance.label = value.val.wkt
-                document.geometries.append(entityinstance)
-            elif entitytype.businesstablename == 'uniqueids':
-                entityinstance.label = idvalue
-                entityinstance.value = idvalue
-                document.child_entities.append(entityinstance)
-            elif entitytype.businesstablename == 'strings':
-                entityinstance.label = archesmodels.Strings.objects.get(entityid = entity.entityid).val
-                entityinstance.value = archesmodels.Strings.objects.get(entityid = entity.entityid).val 
-                document.child_entities.append(entityinstance)
-            else:
-                document.child_entities.append(entityinstance)
-                
-#             print "Entityid: %s Entitytypeid: %s parentid: %s, property %s, businesstable: %s, label: %s, value: %s" % (entityinstance.entityid, entityinstance.entitytypeid,entityinstance.parentid, entityinstance.property, entityinstance.businesstablename, entityinstance.label, entityinstance.value)
-#         if relations:
-            find_nested(document,relation.entityidrange, topId, entitytype, idvalue)
-#         else:
-    
-#         
-
-    find_nested(document,entityid1,entityid1,document.entitytypeid,value)
-    document=  JSONSerializer().serializeToPython(document)
-    
-    se = SearchEngineFactory().create()
-
-
-
-#   Document Index
-    se.index_data('entity', 'HERITAGE_RESOURCE_GROUP.E27', document, id=entityid1)
-
-#   Term index
-    print "TERM value, entityid, context: %s,%s,%s" % (value,entityid2,archesmodels.EntityTypes.objects.get(pk = "EAMENA_ID.E42").conceptid_id)
-    term = {'term': value, 'entityid': entityid2, 'context': archesmodels.EntityTypes.objects.get(pk = "EAMENA_ID.E42").conceptid_id, 'options': {}}
-    se.index_term(term['term'], term['entityid'], term['context'], term['options'])
-
-
+        if len(errors) > 0:
+            print errors[0], ':', len(errors)
