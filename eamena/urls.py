f4c58942ddcdc2de0002e92c45a3918a4adb30ba
azerbini <andrea.zerbini@arch.ox.ac.uk>
2017-03-17 16:33:40 +0000

    Updated eamena urls.py
    Merging eamena + arches_hip

--- eamena/urls.py
+++ eamena/urls.py
@@ -16,13 +16,16 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from arches_hip import urls as arches_hip_urls
+from arches import urls as arches_urls
 from django.conf.urls import patterns, url, include
 
 uuid_regex = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
 
 urlpatterns = patterns('',
-
-    url(r'', include(arches_hip_urls)),
+    url(r'^search$', 'eamena.views.search.home_page', name="search_home"),
+    url(r'^search/resources$', 'eamena.views.search.search_results', name="search_results"),
+    url(r'^reports/(?P<resourceid>%s)$' % uuid_regex , 'eamena.views.resources.report', name='report'),
+    
+    url(r'', include(arches_urls)),
 )
 
