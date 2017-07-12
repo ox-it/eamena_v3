e3adffb360818a18d86562f4de87e6747ce12f9c
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2017-07-12 11:07:12 +0100

    prepare the object needed to show the chart

--- eamena/eamena/media/js/group-chart.js
+++ eamena/eamena/media/js/group-chart.js
@@ -5,15 +5,89 @@ require([
 ], function($, _, Highcharts) {
     // var seconds = new Date().getTime() / 1000;
     var data = {};
+    var chartOptions = {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'USD to EUR exchange rate over time'
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+    
+        // series: [{
+        //     type: 'area',
+        //     name: 'USD to EUR',
+        //     data: data
+        // }]
+    };
     console.log("activitySummary", activitySummary);
 
-    Object.keys(activitySummary).forEach(function (date) {
-        var dateInSeconds = new Date(date).getTime() / 1000;
-        data[dateInSeconds] = activitySummary[date].insert
-        data[dateInSeconds] += activitySummary[date].update;
-        data[dateInSeconds] += activitySummary[date].create;
-        data[dateInSeconds] += activitySummary[date].delete;
+    Object.keys(activitySummary).forEach(function (userData) {
+        console.log("userData", activitySummary[userData]);
+        var data = [];
+        Object.keys(activitySummary[userData].data).forEach(function (dateData) {
+            console.log("dateData", userData.data[dateData]);
+            var  x= {};
+            // item.type = type;
+            // item.name = input[type];
+            // output.push(item);
+        });
+        chartOptions.series = [{
+            type: 'area',
+            name: 'USD to EUR',
+            data: data
+        }]
+        Highcharts.chart('user-chart-' + userData, chartOptions.series);
     });
+
+
+    // Object.keys(activitySummary).forEach(function (date) {
+    //     var dateInSeconds = new Date(date).getTime() / 1000;
+    //     data[dateInSeconds] = activitySummary[date].insert
+    //     data[dateInSeconds] += activitySummary[date].update;
+    //     data[dateInSeconds] += activitySummary[date].create;
+    //     data[dateInSeconds] += activitySummary[date].delete;
+    // });
     
 
     // Highcharts.chart('group-chart', {
