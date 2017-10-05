4f0e4090232ddb68a5606421998f8b67b348948c
Andrew Haith <andyhaith@gmail.com>
2017-10-05 15:52:57 +0100

    add missing python file

---
+++ eamena/eamena/testpdf.py
@@ -0,0 +1,31 @@
+import cStringIO as StringIO
+from django.template.loader import get_template
+from django.template import RequestContext
+
+from wkhtmltopdf.views import PDFTemplateView, PDFTemplateResponse
+
+
+# class MyPDF(PDFTemplateView):
+#     filename = 'report.pdf'
+#     template_name = 'resource-report.htm'
+
+def render_to_pdf(template_src, context_dict, req):
+    template = get_template(template_src)
+    context = RequestContext(req, context_dict)
+    # html = template.render(context)
+    # result = StringIO.StringIO()
+    
+    # pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+    # if not pdf.err:
+    #     return HttpResponse(result.getValue(), content_type='application/pdf')
+    # return HttpResponse('We had some errors<pre>%s</pre> % escape(html)')
+    
+    response = PDFTemplateResponse(
+        request=req,
+        template=template,
+        filename='report.pdf',
+        context=context,
+        cmd_options={'javascript-delay':2000}
+    )
+    
+    return response
