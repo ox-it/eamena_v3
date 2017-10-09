2b612079dcce1b4804faefec8a824c4cd36f3e7d
Andrew Haith <andyhaith@gmail.com>
2017-10-09 09:53:59 +0100

    Code tidy

--- eamena/eamena/pdfutil.py
+++ eamena/eamena/pdfutil.py
@@ -5,27 +5,16 @@ from django.template import Context, RequestContext
 from wkhtmltopdf.views import PDFTemplateView, PDFTemplateResponse
 
 
-# class MyPDF(PDFTemplateView):
-#     filename = 'report.pdf'
-#     template_name = 'resource-report.htm'
-
 def render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
     context = Context(context_dict)
-    # html = template.render(context)
-    # result = StringIO.StringIO()
-    
-    # pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
-    # if not pdf.err:
-    #     return HttpResponse(result.getValue(), content_type='application/pdf')
-    # return HttpResponse('We had some errors<pre>%s</pre> % escape(html)')
     
     response = PDFTemplateResponse(
         request=None,
         template=template,
         filename='report.pdf',
         context=context,
-        cmd_options={'javascript-delay':5000}
+        cmd_options={'javascript-delay':5000}       # allow time for all images to load before taking snapshot
     )
     
     return response
