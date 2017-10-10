20ced6384ab35a0918de8e8be1937c57e1cffc7a
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2017-10-10 10:57:28 +0100

    fix the related resources search

--- eamena/eamena/media/js/views/search/term-filter.js
+++ eamena/eamena/media/js/views/search/term-filter.js
@@ -42,13 +42,15 @@ define(['jquery', 'backbone', 'arches', 'select2', 'knockout'], function ($, Bac
                     data: function (term, page) {
                         return {
                             q: term, // search term
-                            group_root_node: self.termFilterGroup,
+                            group_root_node: self.termFilterGroup || 'No group',
                             page_limit: 30
                         };
                     },
                     results: function (data, page) {
                         var value = $('div.resource_search_widget'+this.index).find('.select2-input').val();
-
+                        if (!value) {
+                            value = $('div.resource_search_widget').find('.select2-input').val();
+                        }
                         // this result is being hidden by a style in arches.css 
                         // .select2-results li:first-child{
                         //     display:none;
