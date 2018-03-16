b5de98d055ac5a78bcb8436ed90820e0f11ecc20
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2018-03-16 15:57:23 +0000

    save multiple nodes in E24 wizard

--- eamena/eamena/models/forms.py
+++ eamena/eamena/models/forms.py
@@ -314,18 +314,8 @@ class ManMadeForm(ResourceForm):
 
         resource = Resource()
         resource.entitytypeid = 'HERITAGE_FEATURE.E24'
-        for node in data['NAME.E41'][0]['nodes']:
-            if node['entitytypeid'] == "NAME.E41":
-                resource.set_entity_value('NAME.E41', node['value'])
-            if node['entitytypeid'] == "NAME_TYPE.E55":
-                resource.set_entity_value('NAME_TYPE.E55', node['value'])
-                
-        for node in data['INVESTIGATION_ASSESSMENT_ACTIVITY.E7'][0]['nodes']:
-            if node['entitytypeid'] == "INVESTIGATOR_ROLE_TYPE.E55":
-                resource.set_entity_value('INVESTIGATOR_ROLE_TYPE.E55', node['value'])
-            if node['entitytypeid'] == "ASSESSMENT_ACTIVITY_TYPE.E55":
-                resource.set_entity_value('ASSESSMENT_ACTIVITY_TYPE.E55', node['value'])
-            
+        self.update_nodes('NAME.E41', data, resource)
+        self.update_nodes('INVESTIGATION_ASSESSMENT_ACTIVITY.E7', data, resource)
         resource.save()
         resource.index()
         relationships = Concept().get_e55_domain('ARCHES_RESOURCE_CROSS-REFERENCE_RELATIONSHIP_TYPES.E55')
@@ -337,6 +327,24 @@ class ManMadeForm(ResourceForm):
         se.index_data(index='resource_relations', doc_type='all', body=model_to_dict(relationship), idfield='resourcexid')
 
         return
+
+    def update_nodes(self, entitytypeid, data, resource):
+        resource.prune(entitytypes=[entitytypeid])
+        if self.schema == None:
+            self.schema = Entity.get_mapping_schema(resource.entitytypeid)
+        for value in data[entitytypeid]:
+            self.baseentity = None
+            for newentity in value['nodes']:
+                entity = Entity()
+                entity.create_from_mapping(resource.entitytypeid, self.schema[newentity['entitytypeid']]['steps'], newentity['entitytypeid'], newentity['value'], newentity['entityid'])
+                if self.baseentity == None:
+                    self.baseentity = entity
+                else:
+                    self.baseentity.merge(entity)
+
+            resource.merge_at(self.baseentity, resource.entitytypeid)
+
+        self.resource.trim()
 
     def load(self, lang):
         data = []
