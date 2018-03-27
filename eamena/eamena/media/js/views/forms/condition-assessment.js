03aecda231b590e2d148a426929269afab1b6812
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2018-03-27 15:18:02 +0100

    changes to the condition assessment files

--- eamena/eamena/media/js/views/forms/condition-assessment.js
+++ eamena/eamena/media/js/views/forms/condition-assessment.js
@@ -8,7 +8,6 @@ define(['jquery',
         return BaseForm.extend({
             initialize: function() {
                 BaseForm.prototype.initialize.apply(this);                
-                
                 var self = this;
                 var date_picker = $('.datetimepicker').datetimepicker({pickTime: false});
                 date_picker.on('dp.change', function(evt){
